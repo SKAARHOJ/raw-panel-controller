239ed52b162200bde7b91f1a229980f353afdeef
Claudio Cabral <cabral1349@gmail.com>
2019-09-10 11:28:23 +0200

    add console component

---
+++ src/renderer/components/Console.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="console">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'console'
+}
+</script>
+
+<style scoped>
+#console {
+  width: 95%;
+  height: 95%;
+  border: 5px black solid;
+  border-radius: 5px;
+  box-sizing: border-box;
+  margin: auto;
+  grid-area: console;
+}
+</style>
